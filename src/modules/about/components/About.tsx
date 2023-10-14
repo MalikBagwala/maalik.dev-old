b0b074a9114fdaeface900babedda2228ccaddef
Malik Bagwala <m.bagwala@gmail.com>
2023-10-14 08:24:46 +0530

    feat: update loop

--- src/modules/about/components/About.tsx
+++ src/modules/about/components/About.tsx
@@ -2,6 +2,7 @@ import Breakline from '@/common/components/elements/Breakline';
 
 import CareerList from './CareerList';
 import Resume from './Resume';
+import Skills from './Skills';
 import Story from './Story';
 
 const About = () => {
@@ -9,6 +10,8 @@ const About = () => {
     <>
       <Story />
       <Resume />
+      <Breakline className='my-8' />
+      <Skills />
       <Breakline className='my-8' />
       <CareerList />
     </>
