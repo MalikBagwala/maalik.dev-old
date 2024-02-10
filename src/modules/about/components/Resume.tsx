f2fbdf9e058a31683335a71f3ab80af9b62222a7
Malik Bagwala <m.bagwala@gmail.com>
2024-02-10 08:26:28 +0000

    feat: change resume link

--- src/modules/about/components/Resume.tsx
+++ src/modules/about/components/Resume.tsx
@@ -4,7 +4,7 @@ import { LuDownload as DownloadIcon } from 'react-icons/lu';
 const Resume = () => {
   return (
     <Link
-      href='https://api.maalik.dev/resume'
+      href='https://links.maalik.dev/resume'
       target='_blank'
       passHref
       className='flex gap-2 hover:gap-3 transition-all duration-300 items-center text-neutral-600 dark:text-neutral-400 hover:text-neutral-700 hover:dark:text-neutral-300 mt-6 border border-neutral-400 hover:border-neutral-500 w-fit px-4 py-2.5 rounded-lg dark:border-neutral-600 hover:dark:border-neutral-300 dark:bg-neutral-900'
