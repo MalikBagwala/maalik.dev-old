75001667412f8951421fabb170da577d26505c1f
Malik Bagwala <m.bagwala@gmail.com>
2024-06-26 17:00:28 +0530

    chore: update api endpoint

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -1,4 +1,10 @@
 import axios from 'axios';
 
 export const fetcher = (url: string) =>
-  axios.get(url).then((response) => response.data);
+  axios
+    .get(url, {
+      headers: {
+        origin: 'maalik.dev',
+      },
+    })
+    .then((response) => response.data);
