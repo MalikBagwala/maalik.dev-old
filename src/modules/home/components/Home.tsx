4f15f3071803e7abc58071637dfb146d3da687c1
Malik Bagwala <m.bagwala@gmail.com>
2024-06-30 09:08:02 +0530

    chore: add skills to the main page

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -2,11 +2,15 @@ import Breakline from '@/common/components/elements/Breakline';
 
 import Introduction from './Introduction';
 import Services from './Services';
+import Skills from '@/modules/about/components/Skills';
 
 const Home = () => {
   return (
     <>
       <Introduction />
+
+      <Breakline className='mt-8 mb-6' />
+      <Skills />
       <Breakline className='mt-8 mb-6' />
       <Services />
     </>
