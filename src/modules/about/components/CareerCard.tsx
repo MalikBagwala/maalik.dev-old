394a863b459df2d0ce0da01822b266b524030967
Malik Bagwala <m.bagwala@gmail.com>
2024-07-20 23:10:52 +0530

    chore: remove responsibilities section if there are none

--- src/modules/about/components/CareerCard.tsx
+++ src/modules/about/components/CareerCard.tsx
@@ -6,7 +6,7 @@ import Card from '@/common/components/elements/Card';
 import CustomImage from '@/common/components/elements/CustomImage';
 import { durationBetweenDates, formatDate } from '@/common/helpers';
 import { CareerProps } from '@/common/types/careers';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import clsx from 'clsx';
 dayjs.extend(duration);
@@ -90,35 +90,39 @@ const CareerCard = ({
             </div>
           </div>
         </div>
-        <button
-          onClick={() => setIsShowResponsibility(!isShowResponsibility)}
-          className='-ml-1 mt-5 flex items-center gap-1 text-sm text-neutral-500'
-        >
-          <HiChevronRight
-            size={18}
-            className={clsx({
-              'rotate-90 transition-all duration-300': isShowResponsibility,
-            })}
-          />
-          {isShowResponsibility ? 'Hide' : 'Show'} Responsibilities
-        </button>
-        <AnimatePresence>
-          {isShowResponsibility && (
-            <motion.ul
-              className='ml-5 list-disc space-y-1 pb-2 text-sm leading-normal text-neutral-600 dark:text-neutral-400'
-              initial={{ y: -20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: -20, opacity: 0 }}
-              transition={{ duration: 0.3, ease: 'easeInOut' }}
+        {responsibilities && responsibilities?.length > 0 ? (
+          <Fragment>
+            <button
+              onClick={() => setIsShowResponsibility(!isShowResponsibility)}
+              className='-ml-1 mt-5 flex items-center gap-1 text-sm text-neutral-500'
             >
-              {responsibilities?.map((item) => (
-                <motion.li key={item} layout>
-                  {item}
-                </motion.li>
-              ))}
-            </motion.ul>
-          )}
-        </AnimatePresence>
+              <HiChevronRight
+                size={18}
+                className={clsx({
+                  'rotate-90 transition-all duration-300': isShowResponsibility,
+                })}
+              />
+              {isShowResponsibility ? 'Hide' : 'Show'} Responsibilities
+            </button>
+            <AnimatePresence>
+              {isShowResponsibility && (
+                <motion.ul
+                  className='ml-5 list-disc space-y-1 pb-2 text-sm leading-normal text-neutral-600 dark:text-neutral-400'
+                  initial={{ y: -20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -20, opacity: 0 }}
+                  transition={{ duration: 0.3, ease: 'easeInOut' }}
+                >
+                  {responsibilities?.map((item) => (
+                    <motion.li key={item} layout>
+                      {item}
+                    </motion.li>
+                  ))}
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </Fragment>
+        ) : null}
       </div>
     </Card>
   );
